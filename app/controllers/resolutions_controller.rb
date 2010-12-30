75419d1918bef71b27323d882a11fe0c89b600b6
B V Satyaram <bvsatyaram@gmail.com>
2010-12-30 17:14:15 +0530

    Skip authenticity token for requests from facebook

--- app/controllers/resolutions_controller.rb
+++ app/controllers/resolutions_controller.rb
@@ -1,4 +1,6 @@
 class ResolutionsController < ApplicationController
+  skip_before_filter :verify_authenticity_token if :request_from_facebook?
+  
   def suggest
     @resolution = Resolution.first(:order => 'RANDOM()')
   end
